33dd3a1748457aea439b4e2f43db7b2bb768fbfe
Finn Sinclair <fns2@illinois.edu>
2018-09-18 16:46:34 -0500

    new 8 bit conversions

--- Lab4/Processor.sv
+++ Lab4/Processor.sv
@@ -10,11 +10,11 @@ module Processor (input logic   Clk,     // Internal
                                 LoadA,   // Push button 1
                                 LoadB,   // Push button 2
                                 Execute, // Push button 3
-                  input  logic [3:0]  Din,     // input data
+                  input  logic [7:0]  Din,     // input data
                   input  logic [2:0]  F,       // Function select
                   input  logic [1:0]  R,       // Routing select
                   output logic [3:0]  LED,     // DEBUG
-                  output logic [3:0]  Aval,    // DEBUG
+                  output logic [7:0]  Aval,    // DEBUG
                                 Bval,    // DEBUG
                   output logic [6:0]  AhexL,
                                 AhexU,
@@ -27,7 +27,7 @@ module Processor (input logic   Clk,     // Internal
 	 logic [1:0] R_S;
 	 logic Ld_A, Ld_B, newA, newB, opA, opB, bitA, bitB, Shift_En,
 	       F_A_B;
-	 logic [3:0] A, B, Din_S;
+	 logic [7:0] A, B, Din_S;
 	 
 	 
 	 //We can use the "assign" statement to do simple combinational logic
@@ -81,10 +81,10 @@ module Processor (input logic   Clk,     // Internal
 								
 	 //When you extend to 8-bits, you will need more HEX drivers to view upper nibble of registers, for now set to 0
 	 HexDriver        HexAU (
-                        .In0(4'h0),
+                        .In0(A[7:4]),
                         .Out0(AhexU) );	
 	 HexDriver        HexBU (
-                       .In0(4'h0),
+                       .In0(B[7:4]),
                         .Out0(BhexU) );
 								
 	  //Input synchronizers required for asynchronous inputs (in this case, from the switches)
@@ -92,7 +92,7 @@ module Processor (input logic   Clk,     // Internal
 	  //Note: S stands for SYNCHRONIZED, H stands for active HIGH
 	  //Note: We can invert the levels inside the port assignments
 	  sync button_sync[3:0] (Clk, {~Reset, ~LoadA, ~LoadB, ~Execute}, {Reset_SH, LoadA_SH, LoadB_SH, Execute_SH});
-	  sync Din_sync[3:0] (Clk, Din, Din_S);
+	  sync Din_sync[7:0] (Clk, Din, Din_S);
 	  sync F_sync[2:0] (Clk, F, F_S);
 	  sync R_sync[1:0] (Clk, R, R_S);
 	  
