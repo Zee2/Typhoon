2c2de58fdb2b9c9e7329bb9dc07d9e1bd447e652
Finn Sinclair <fns2@illinois.edu>
2018-11-07 13:31:55 -0600

    Potential fork point for T-tables hack

--- lab9/AES.sv
+++ lab9/AES.sv
@@ -17,5 +17,72 @@ module AES (
 	output logic [127:0] AES_MSG_DEC
 );
 
+	
+logic [3:0] loopCounter = 0;
+logic [1407:0] RoundKeySchedule;
+
+KeyExpansion keyExpander(
+	.clk(CLK),
+	.Cipherkey(AES_KEY),
+	.KeySchedule(RoundKeySchedule)
+);
+
+enum logic [7:0] {
+
+	waiting,
+	done,
+	AESbegin,
+	expansion
+	
+	
+
+} state, nextState;
+
+
+
+
+// Synchronous state latching
+always_ff @ (posedge CLK) begin
+	state <= nextState;
+
+end
+
+// Combinatorial next state calcs
+always_comb begin
+
+	case (state)
+	
+		waiting: begin
+			if(AES_START == 0)
+				nextState = waiting;
+			else
+				nextState = expansion;
+		end
+		
+		
+		done: begin
+			if(AES_START == 0)
+				nextState = waiting;
+			else
+				nextState = done;
+		
+		end
+		
+		expansion: begin
+			if(loopCounter >= 10)
+				nextState = expansion;
+			else
+				nextState = AESbegin;
+			
+		end
+		
+		default:
+			nextState = waiting;
+		
+	
+	endcase
+
+end
+
 
 endmodule
